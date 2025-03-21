91bdec20f66130952dbbd4ee48aa520aa4517eaf
sunnySavlot <sunny.savlot@gmail.com>
2025-03-21 13:43:37 +0530

    props added to txt

--- src/components/FancyTxt.tsx
+++ src/components/FancyTxt.tsx
@@ -1,4 +1,4 @@
-export default function FancyTxt({ children }) {
+export default function FancyTxt({ children }: { children: React.ReactNode }) {
   return (
     <span className="border-b border-b-[#18CBE4] text-[#18CBE4]">
       {children}
